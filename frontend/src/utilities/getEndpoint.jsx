8c883bd9dbe3aa1f1e83df19b1287c5b990b072f
Ben Baker <bakerb845@gmail.com>
2024-08-28 11:51:02 -0600

    doesn't matter if the client knows the end point.  b/c well, they have to

--- frontend/src/utilities/getEndpoint.jsx
+++ frontend/src/utilities/getEndpoint.jsx
@@ -1,8 +1,8 @@
 export default function getEndpoint() {
   var apiEndpoint = 'http://127.0.0.1:8080/';
   if (import.meta.env.mode === 'production') {
-    if (import.meta.env.production.CCT_ENDPOINT !== '') {
-      apiEndpoint = import.meta.env.production.CCT_ENDPOINT; 
+    if (import.meta.env.production.VITE_CCT_ENDPOINT !== '') {
+      apiEndpoint = import.meta.env.production.VITE_CCT_ENDPOINT; 
     }
   }
   return apiEndpoint;
