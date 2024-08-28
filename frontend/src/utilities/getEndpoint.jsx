6c5a50af58b4aa1676d8bbd838b55e74580f54c3
Ben Baker <bakerb845@gmail.com>
2024-08-28 12:17:19 -0600

    process undefined - use vite way?

--- frontend/src/utilities/getEndpoint.jsx
+++ frontend/src/utilities/getEndpoint.jsx
@@ -1,5 +1,5 @@
 export default function getEndpoint() {
-  const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_DEV_API_URL;
+  const apiUrl = import.meta.env.NODE_ENV === 'production' ? import.meta.env.REACT_APP_PROD_API_URL : import.meta.env.REACT_APP_DEV_API_URL;
   return apiUrl;
 };
 
