5d655ffec7ab2e457c53b65a4305fa58abb10c8a
Ben Baker <bakerb845@gmail.com>
2024-12-02 14:10:45 -0700

    updating the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import { Box, Button, Center, Flex, Heading, Text } from '@chakra-ui/react';
-import SettingsDialog from '/src/components/SettingsDialog.jsx';
-import { useDisclosure } from '@chakra-ui/react';
+import { Center, Flex, Heading, Text } from '@chakra-ui/react';
+import SettingsDialog from '/src/components/SettingsDialog';
 
 function Header( {eventIdentifier, settings, onUpdateSettings, onLogout} ) {
   console.log("Rendering header...");
 
-  const text = `Mw,Coda Review: ${eventIdentifier}`;
+  const text = eventIdentifier != '' ? `Event: ${eventIdentifier}` : 'Mw,Coda Review';
   return (
     <React.Fragment>
       <Flex color='white' w='100%' h='60px'>
-        <Center w='35%' bg='black' color='white'>
-          <Heading fontSize='28px' color='white' align='left'>Mw,Coda Review: {eventIdentifier}</Heading>
+        <Center w='20%' bg='black' color='white'>
+          <Heading fontSize='26px' color='white' align='left'>
+           <Text>{text}</Text>
+          </Heading>
         </Center>
-        <Center w='50%' bg='black'/>
+        <Center w='65%' bg='black'/>
         <Center w='15%' bg='black' color='white'>
-          { /* <Button leftIcon={<MdBuild />} colorScheme='blue' variant='solid'> */ }
-          { /* Settings */ }
-          { /* onClick={isSettingsOpen} */ }
-          { /*</Button> */}
           <SettingsDialog
            settings={settings}
            onUpdateSettings={onUpdateSettings}
@@ -26,9 +23,6 @@ function Header( {eventIdentifier, settings, onUpdateSettings, onLogout} ) {
           />
         </Center>
       </Flex>
-      { /* <header className="header"> */ }
-      { /* <h1>{text}</h1> */ }
-      { /* </header> */ }
     </React.Fragment>
   );
 }
