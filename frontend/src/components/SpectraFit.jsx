2998ff00ea0ab50f5f04b283693058f87179a0ba
Ben Baker <bakerb845@gmail.com>
2025-02-11 13:39:33 -0700

    making the dots colorful

--- frontend/src/components/SpectraFit.jsx
+++ frontend/src/components/SpectraFit.jsx
@@ -1,5 +1,34 @@
 import * as React from "react";
+//import cividis from '/src/utilities/cividis';
 import { VictoryAxis, VictoryChart, VictoryLine, VictoryScatter, VictoryTooltip, VictoryTheme } from "victory";
+
+import cividis from '/src/utilities/cividis';
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(rgb) {
+  return "#" + componentToHex(rgb[0]) + componentToHex(rgb[1]) + componentToHex(rgb[2]);
+}
+
+function stationColorMap(stationMeasurements, colorMap) {
+  const nStations = stationMeasurements.length;
+  if (stationMeasurements.length == 0){return stationMeasurements;}
+  if (stationMeasurements.length == 1) {
+    stationMeasurements[0].color = '#000000';
+  }
+  const nColors = colorMap.length;
+  const interval = nColors/(nStations - 1);
+  console.info(nStations, interval);
+  for (var i = 0; i < stationMeasurements.length; i++) {
+    const idx = Math.max(0, Math.min(nColors - 1, Math.floor(interval*i)));
+    stationMeasurements[i].color = rgbToHex(colorMap[idx]);
+  }
+  return stationMeasurements;
+}
+
 
 function SpectraFit( props ) {
   var fitLine = null;
@@ -23,7 +52,8 @@ function SpectraFit( props ) {
       upperBound.push( {x: spectralFit['bruneUpperBound-2'].frequencies[i],
                         y: spectralFit['bruneUpperBound-2'].values[i]} );
     }
-    const stationMeasurements = props.data.stationMeasurements;
+    const stationMeasurements = stationColorMap(props.data.stationMeasurements, cividis);
+    //stationColorMap(stationMeasurements, cividis);
     for (var i = 0; i < stationMeasurements.length; i++) {
       var points = [];
       for (var j = 0; j < stationMeasurements[i].measurements.length; j++) {
@@ -31,12 +61,14 @@ function SpectraFit( props ) {
         points.push({x: stationMeasurements[i].measurements[j].centerFrequency,
                      y: stationMeasurements[i].measurements[j].value,
                      label: `${stationMeasurements[i].station}: ${residual}`,
+                     color: stationMeasurements[i].color,
                      size: 3});
       }
       var measurement = {id: stationMeasurements[i].station,
                          points: points.slice()};
       measurements.push(measurement);
     }
+    
   }
   return (
     <React.Fragment>
@@ -91,7 +123,15 @@ function SpectraFit( props ) {
         />
         {measurements.map( (measurement) => (
              <VictoryScatter
-              labelComponent={<VictoryTooltip style={{fontSize: '8px'}}/>}
+              labelComponent={<VictoryTooltip style={{fontSize: '8px', }}/>}
+              style={{
+                      data: {
+                      fill: ({ datum }) => datum.color,
+                      stroke: 'black',
+                      fillOpacity: 1.0,
+                      strokeWidth: 0.2,
+                     },
+                   }}
               size={3}
               key={measurement.id}
               data={measurement.points}
